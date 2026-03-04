93ef323c4d06620364fe745394216a459c74f691
Himanshu Gaura <himanshu32505@gmail.com>
2026-03-04 23:27:01 +0530

    zod setup

---
+++ backend/src/middlewares/validate.ts
@@ -0,0 +1,21 @@
+import { ZodObject } from "zod";
+import type { Request, Response, NextFunction } from "express";
+
+export const validate =
+  (schema: ZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+
+      next();
+    } catch (error: any) {
+      return res.status(400).json({
+        success: false,
+        errors: error.errors,
+      });
+    }
+  };
